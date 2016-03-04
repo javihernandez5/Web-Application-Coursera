3ff412cff105b021852a51d6a51541a07163a127
javihernandez5 <javihm555@gmail.com>
2016-03-04 20:38:24 +0100

    d
    f

---
+++ site/algo.c
@@ -0,0 +1,23 @@
+##include "algo.h"
+int main(int argc, char const *argv[])
+{
+	int ca;
+	int count;
+	int * c = (int*)malloc(2*sizeof(int));
+
+	c={1,2};
+
+	
+	if (c ==0)
+	{
+		/* code */
+
+	}
+	for (int i = 0; i < count; ++i)
+	{
+		/* code */
+
+		c++;
+	}
+	return 0;
+}
